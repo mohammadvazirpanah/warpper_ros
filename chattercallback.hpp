964a20ef846ed6c62f448f1efdf845211c0544d6
Mohammad <mohammad.vazirpanah@yahoo.com>
2021-05-17 06:49:54 +0430

    Final Change

--- chattercallback.hpp
+++ chattercallback.hpp
@@ -1,12 +1,19 @@
-/*#include <systemc>
-
-SC_MODULE(chatterCallback)
-{
-
-}*/
-
-
+//void chatterCallback (const ros::MessageEvent<std_msgs::String const>& event)
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
- 	ROS_INFO("I heard: [%s]", msg->data.c_str());
+	
+	
+		//ROS_INFO("I heard: [%s]", msg->data.c_str());
+	
+
+	//const std::string& publisher_name = event.getPublisherName();
+   	//const ros::M_string& header = event.getConnectionHeader();
+   	//ros::Time receipt_time = event.getReceiptTime();
+   	//const std_msgs::StringConstPtr& msg = event.getMessage();
+
+	ROS_INFO("I heard: [%s]", msg->data.c_str());
+
+
+	
+ 	
 }
